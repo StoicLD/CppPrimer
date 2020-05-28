535a327d11392089523692dcafaa4b9c46ace26e
Lidong <729790146@qq.com>
2020-05-28 15:03:51 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/CppPrimer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppPrimer.dir/main.cpp.obj"
+  "CppPrimer.exe"
+  "CppPrimer.exe.manifest"
+  "CppPrimer.lib"
+  "CppPrimer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppPrimer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CppPrimer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CppPrimer.
+# This may be replaced when dependencies are built.
